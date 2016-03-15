d27ecaf942bb52b8d6907e985242348750a77ac2
Mei08 <125234mary@gmail.com>
2016-03-15 00:11:36 -0400

    first commit

---
+++ config/db.js
@@ -0,0 +1,10 @@
+// create public connection string
+module.exports = {
+
+    // local
+    // 'url': 'mongodb://localhost/test'
+
+    // live on mLab
+    'url': 'mongodb://mei:123456@ds064278.mlab.com:64278/2106'
+    
+};
